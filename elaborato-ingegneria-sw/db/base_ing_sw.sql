3c77791203e993216a298862b3eddea065987d53
elped <elped@DESKTOP-N3PV3C7>
2022-07-06 17:38:58 +0200

    modifica db

--- elaborato-ingegneria-sw/db/base_ing_sw.sql
+++ elaborato-ingegneria-sw/db/base_ing_sw.sql
@@ -5,47 +5,74 @@ CREATE DOMAIN patenti AS VARCHAR(4)
 CREATE DOMAIN spe_esp AS VARCHAR(20)
 	CHECK( VALUE IN ('bagnino', 'barman', 'istruttore di nuoto', 'viticultore', 'floricultore'));
 
-CREATE TABLE lavoratore (
-	--dati anagrafici
+CREATE TABLE Lavoratore (
 	id_lavoratore SERIAL, 
-	luogo_di_nascita VARCHAR(30) NOT NULL, 
-	--potrei anche creare una tabella a parte con luogo di nascita (con un id)
-	data_di_nascita DATE NOT NULL,--controllo da svolgere su java
-	nazionalit√† VARCHAR(30) NOT NULL,
-	--
+	id_anagrafica SERIAL REFERENCES Anagrafica(id_anagrafica),
+	id_recapito_urgenza SERIAL REFERENCES Recapito(id_recapito),,
 	indirizzo VARCHAR(30) DEFAULT '',
-	telefono VARCHAR(12) CHECK (telefono SIMILAR TO '\+?[0-9]+')) DEFAULT '',
-	email VARCHAR(30) UNIQUE NOT NULL,
 	specia_esp spe_esp DEFAULT '',
-	-- lo farei separato (+ inserimenti per lavoratore)
-	lingue_parlate VARCHAR(20) DEFAULT 'Italiano',
-	-- separato (+ inserimenti per lavoratore)
-	tipo_patente patenti, --controllo tipo patente con java oppure creo un domain
 	automunito BOOLEAN,
 	--PRIMARY KEY(nome,cognome, luogo_di_nascita, data_di_nascita, nazionalit√†);
 	PRIMARY KEY(id_lavoratore)
 );
 
-CREATE TABLE zona(
-	periodi INTERVAL, 
-	comuni VARCHAR(30),
-	lavoratore SERIAL REFERENCES lavoratore (id_lavoratore)
+CREATE TABLE Disponibilit‡(
+	id_disponibilit‡ SERIAL,
+	id_lavoratore SERIAL REFERENCES lavoratore (id_lavoratore),
+	periodo INTERVAL, 
+	comune VARCHAR(30),
+	PRIMARY KEY (id_disponibilit‡)
 );
 
-CREATE TABLE persona(
+CREATE TABLE Recapito(
+	id_recapito SERIAL,
 	nome VARCHAR(30) NOT NULL,
 	cognome VARCHAR(30) NOT NULL,
 	telefono VARCHAR(12) CHECK (telefono SIMILAR TO '\+?[0-9]+') NOT NULL,
 	email VARCHAR(30) UNIQUE NOT NULL,
-	lavoratore SERIAL REFERENCES lavoratore (id_lavoratore)
+	PRIMARY KEY (id_recapito)
 );
 
 CREATE TABLE lavoro_svolto(
-	periodo INTERVAL,
+	id_lavoro_svolto SERIAL,
+	id_lavoratore SERIAL REFERENCES lavoratore(id_lavoratore),
+	periodo DATE INTERVAL,
 	nome_azienda VARCHAR(30),
 	mansione_svolta VARCHAR(30),
 	luogo_di_lavoro VARCHAR(30),
 	retri_lorda_giornaliera DECIMAL(8,2),
+);
+
+CREATE TABLE Anagrafica(
+	id_anagrafica SERIAL,
+	luogo_di_nascita VARCHAR(30),
+	data_di_nascita DATE NOT NULL,
+	nazionalit‡ VARCHAR(30) NOT NULL,
+	nome VARCHAR(30) NOT NULL,
+	cognome VARCHAR(30) NOT NULL,
+	telefono VARCHAR(12) CHECK (telefono SIMILAR TO '\+?[0-9]+')) DEFAULT '',
+	email VARCHAR(30) UNIQUE,
+	PRIMARY KEY (id_anagrafica)
+);
+
+CREATE TABLE Lavoratore_Patente(
+	id_patente SERIAL REFERENCES Patente(id_patente),
+	id_lavoratore SERIAL REFERENCES Lavoratore(id_lavoratore)
+);
+
+CREATE TABLE Patente(
+	id_patente SERIAL PRIMARY KEY,
+	nome_patente patenti
+);
+
+CREATE TABLE Lavoratore_Lingua(
+	id_lingua SERIAL REFERENCES Lingua(id_lingua),
+	id_lavoratore SERIAL REFERENCES Lingua(id_lingua),
+);
+
+CREATE TABLE Lingua(
+	id_lingua SERIAL PRIMARY KEY,
+	nome_lingua VARCHAR(30)
 );
 
 --NB: capire il funzionamente di serial con references
--- elaborato-ingegneria-sw/db/base_ing_sw_dipendente.sql
+++ elaborato-ingegneria-sw/db/base_ing_sw_dipendente.sql
@@ -1,21 +1,12 @@
 --tabella per i dipendenti
 CREATE TABLE dipendenti (
-	id_dipendente SERIAL --non presente nelle indicazioni
-	nome VARCHAR(30) NOT NULL
-	cognome VARCHAR(30) NOT NULL
-	luogo_di_nascita VARCHAR(30) NOT NULL
-	data_di_nascita DATE NOT NULL
-	nazionalità VARCHAR(30) NOT NULL
-	email VARCHAR(30) UNIQUE NOT NULL
-	telefono VARCHAR(12) CHECK (telefono SIMILAR TO '\+?[0-9]+') 
+	id_dipendente SERIAL, --non presente nelle indicazioni
+	id_anagrafica SERIAL REFERENCES Anagrafica(id_anagrafica),
+	login VARCHAR(30) UNIQUE,
+	password VARCHAR(30), 
 	PRIMARY KEY (id_dipendente)
 );
 
---potrei anche inserirlo in dipendente
-CREATE TABLE credenziali_accesso(
-	login VARCHAR(30) UNIQUE NOT NULL 
-	pw VARCHAR(30) NOT NULL
-	dipendente SERIAL REFERENCES dipendenti(id_dipendente)
-);
+--serial � equivalente a id integer NOT NULL DEFAULT nextval('table_name_id_seq')
 
 --serial è equivalente a id integer NOT NULL DEFAULT nextval('table_name_id_seq')
