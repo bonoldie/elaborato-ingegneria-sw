ed2f721916b8938e3c58ebcc2a47232d5d47e878
elped <elped@DESKTOP-N3PV3C7>
2022-07-11 19:05:24 +0200

    valori da inserire nella tabella patente

---
+++ elaborato-ingegneria-sw/db/base_ing_sw_patente.sql
@@ -0,0 +1,4 @@
+INSERT INTO Patente(nome_patente)
+	VALUES ('AM'),('A1'),('A2'),('A'),('B1'),('B'),('BE'),('C1'),('C1E'),
+	('C'),('CE'),('D1'),('D1E'),('D'),('DE'),('KA'),('KB'),('CQC'), ('CFP'),
+	('NO');
